9637b9d56414a0326ec6f947ebce2b12f18cc459
ViYaYaYa <eureka-studio@qq.com>
2018-03-22 17:39:02 +0800

    看到navigator啦哈哈

--- pages/components/components.js
+++ pages/components/components.js
@@ -2,29 +2,14 @@
 //获取应用实例
 Page({
   data: {
-    scrollView1: 0
+    console: '',
   },
-  doSwiperChange (arg1) {
+  doPickerChange (arg1) {
     console.log(arg1)
-  },
-  doSwiperFinish (arg1) {
-    console.log(arg1)
-  },
-  doScrollViewUpper (arg1, arg2) {
-    console.log(arg1)
-    console.log(arg2)
-  },
-  doScrollViewTop () {
-    setTimeout(function () {
-      this.setData({
-        scrollView1: 300
-      })
-    }.bind(this), 3000)
-  },
-  doScrollViewScroll (ev) {
-    console.log(ev)
+    this.setData({
+      console: arg1
+    })
   },
   onLoad () {
-    this.doScrollViewTop()
   }
 })
