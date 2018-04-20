8d421c2bdb6df36a8e64055bcefd58f19ded998f
ViYaYaYa <eureka-studio@qq.com>
2018-04-20 12:05:54 +0800

    能够动起来了欧耶！

--- pages/play/play.js
+++ pages/play/play.js
@@ -5,15 +5,16 @@ const ORIGIN_X = CANVAS_WIDTH / 2
 const ORIGIN_Y = CANVAS_HEIGHT / 2
 
 const INITIAL_WIDTH = 20
+const INITIAL_SPEED = 1000
 
 class Snake {
   constructor () {
     this.head = new Muscle()
-    this.lv = 0
-    this.direction = 2 // 1上，2右，3下，4左
+    this.lv = 1
+    this.direction = 3 // 1上，2右，3下，4左
   }
-  move (callback) {
-    let { offsetX, offsetY } = getSnakeOffset(this.direction)
+  move () {
+    let { offsetX, offsetY } = this.getSnakeOffset()
     let prevX = this.head.x
     let prevY = this.head.y
     this.head.x += offsetX
@@ -29,11 +30,11 @@ class Snake {
       prevY = nextY
       target = target.next
     }
-    if (typeof callback === 'function') callback()
-    setTimeout(this.move.bind(this, callback), 1000)
+    snakeDraw()
+    setTimeout(this.move.bind(this), INITIAL_SPEED / this.lv)
   }
   grow () {
-    let { offsetX, offsetY } = getSnakeOffset(this.direction)
+    let { offsetX, offsetY } = this.getSnakeOffset()
     this.head = new Muscle(this.head.x + offsetX, this.head.y + offsetY, this.head)
   }
   each (callback) {
@@ -44,11 +45,32 @@ class Snake {
     }
   }
   getHowFat () {
-    let target = this.lv
+    let target = this.lv - 1
     while (ORIGIN_X % (INITIAL_WIDTH - target) !== 0) {
       target++
     }
     return INITIAL_WIDTH - target
+  }
+  getSnakeOffset () {
+    let offsetX, offsetY, direction
+    direction = +this.direction || 1
+    if (direction === 1) {
+      offsetX = 0
+      offsetY = -1
+    }
+    if (direction === 2) {
+      offsetX = 1
+      offsetY = 0
+    }
+    if (direction === 3) {
+      offsetX = 0
+      offsetY = 1
+    }
+    if (direction === 4) {
+      offsetX = -1
+      offsetY = 0
+    }
+    return { offsetX, offsetY }
   }
 }
 
@@ -60,29 +82,27 @@ class Muscle {
   }
 }
 
-function getSnakeOffset (direction) {
-  let offsetX, offsetY
-  if (direction === 1) {
-    offsetX = 0
-    offsetY = -1
-  }
-  if (direction === 2) {
-    offsetX = 1
-    offsetY = 0
-  }
-  if (direction === 3) {
-    offsetX = 0
-    offsetY = 1
-  }
-  if (direction === 4) {
-    offsetX = -1
-    offsetY = 0
-  }
-  return { offsetX, offsetY }
+let ctx = null
+let snake = null
+let drawTasks = 0
+
+function snakeDraw () {
+  drawTasks++
+  setTimeout(function () {
+    drawTasks--
+    if (drawTasks <= 0) {
+      drawTasks = 0
+      ctx.translate(ORIGIN_X, ORIGIN_Y)
+      let fat = snake.getHowFat()
+      snake.each(function (muscle) {
+        ctx.fillRect(muscle.x * fat, muscle.y * fat, fat, fat)
+      })
+      ctx.draw()
+    }
+  }, 0)
 }
 
-// d for Data
-// a for Action
+// do for Action
 // Thank you!
 
 Page({
@@ -90,30 +110,18 @@ Page({
     CANVAS_WIDTH,
     CANVAS_HEIGHT
   },
-  dCtx: null,
-  dSnake: null,
-  aDraw () {
-    this.dCtx.translate(ORIGIN_X, ORIGIN_Y)
-    let fat = this.dSnake.getHowFat()
-    this.dSnake.each(function (muscle) {
-      this.dCtx.fillRect(muscle.x * fat, muscle.y * fat, fat, fat)
-    }.bind(this))
-    this.dCtx.draw()
+  doChangeDirection (ev) {
+    let dir = ev.target.dataset.direction
+    if (snake) {
+      snake.direction = dir
+    }
   },
   onLoad () {
-    this.dCtx = wx.createCanvasContext('canvas')
-    this.dSnake = new Snake()
-    this.dSnake.grow()
-    this.dSnake.grow()
-    // this.dSnake.grow()
-    // this.dSnake.grow()
-    this.dSnake.move(this.aDraw)
-    setInterval(function () {
-      let target = Math.floor(Math.random() * 4 + 1)
-      let curr = this.dSnake.direction
-      if ((target - curr) % 2) {
-        this.dSnake.direction = target
-      }
-    }.bind(this), 2000)
+    ctx = wx.createCanvasContext('canvas')
+    snake = new Snake()
+    snake.grow()
+    snake.grow()
+    snake.grow()
+    snake.move()
   }
 })
